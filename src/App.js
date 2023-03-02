0f607f11ccedebcc380dc07ad161d6d8c5d36251
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-03-02 21:08:52 +0530

    Made the app error proof

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  
+  const [isLoading,setIsLoading]=useState(false);
+  const [error,setError]=useState(null);
+
+  const stopRetryingHandler=()=>{
+    setError(null)
+  }
+
   const fetchMoviesHandler = async () => {
-    const response = await fetch("https://swapi.dev/api/films");
-    const data = await response.json();
-    const transformedData = data.results.map((movie) => {
-      return {
-        id: movie.episode_id,
-        title: movie.title,
-        openingText: movie.opening_crawl,
-        releaseDate: movie.release_date,
-      };
-    });
-    setMovies(transformedData);
+    setIsLoading(true);
+    setError(null);
+
+    try{
+      const response = await fetch("https://swapi.dev/api/films");
+      if(!response.ok){
+        throw new Error('Something went wrong..Retrying')
+      }
+      const data = await response.json();
+      const transformedData = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+      setMovies(transformedData);
+    }catch(error){
+      setError(error.message)
+    }
+    setIsLoading(false)
   };
+
+  useEffect(()=>{
+    if(error){
+      const id=setInterval(() => {
+        fetchMoviesHandler();
+      }, 5000);
+
+      return ()=>{
+        clearInterval(id)
+      }
+    }
+
+  },[error])
+
+
+
+  let content=<p>Found no movies</p>
+  if(movies.length>0){
+    content=<MoviesList movies={movies} />
+  }
+  if(isLoading){
+    content=<p>LOADING...Please Wait</p>
+  }
+  if(error){
+    content=<p>{error} <button onClick={stopRetryingHandler}>Cancle Retrying</button></p>
+  }
 
   return (
     <React.Fragment>
@@ -26,7 +69,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+       {content}
       </section>
     </React.Fragment>
   );
--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// React_Bootstrap
+
+// import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+// import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
